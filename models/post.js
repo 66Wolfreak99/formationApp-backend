4627c3e2eead629228593c2be5e5030d51d9220f
66Wolfreak99 <jeremy.argoud@mb-expansion.fr>
2021-07-06 10:20:17 +0200

    modifié model de post

--- models/post.js
+++ models/post.js
@@ -1,12 +1,17 @@
 const mongoose = require('mongoose');
 const taskSchema = mongoose.Schema({
+    project_id: {type: String, required: true},
     name: {type: String, required: true},
-    isFinished: {type: Boolean}
+    description: { type: String, required: true },
+    created_at: { type: Object, required: true },
+    isComplete: {type: Boolean}
 });
 const postSchema = mongoose.Schema({
     title: {type: String, required:true},
     description: { type: String, required: true },
     userId: { type: String, required: true },
+    created_by: { type: String, required: true },
+    created_at: { type: String, required: true },
     tasks: [taskSchema],
 });
 module.exports = mongoose.model('Post', postSchema);
